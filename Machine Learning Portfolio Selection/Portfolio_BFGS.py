1e0a8ede9db1437cb3cf1e74ee2eadd6c02c8555
pbrock94 <patrick.brock@stud.uni-frankfurt.de>
2025-10-29 14:20:46 +0100

    Fixed stock universe

--- Machine Learning Portfolio Selection/Portfolio_BFGS.py
+++ Machine Learning Portfolio Selection/Portfolio_BFGS.py
@@ -58,11 +58,16 @@ df = pd.read_sql_query(("SELECT id, eom, lambda, tr, tr_ld1, tr_m_sp500, tr_m_sp
 #Data subsets: Kyle's Lambda, returns, portfolio weights
 df_kl, df_returns, df_pf_weights = (df.get(['id','eom','lambda']), 
          df.get(['id','eom','tr','tr_ld1','tr_m_sp500','tr_m_sp500_ld1']),
-         df.get(['id','eom','me'])
+         df.get(['id','eom','me', 'lambda', 'tr'])
          )
 
 #Save Memory
 del df
+
+#Read in wealth
+df_wealth = (pd.read_csv(path + "Data/wealth_evolution.csv", parse_dates = ['eom'])
+             .pipe(lambda df: df.loc[df['eom'] >=trading_start - pd.offsets.MonthEnd(1)])
+             )
 
 #Compute initial value weighted portfolio
 df_pf_weights = (df_pf_weights
@@ -71,12 +76,26 @@ df_pf_weights = (df_pf_weights
     # 2. Calculate the aggregate market cap per date
     .assign(group_sum=lambda df: df.groupby('eom')['me'].transform('sum'))
     # 3. Calculate a value weighted initial portfolio
-    .assign(w=lambda df: df['me'] / df['group_sum'])
+    .assign(pi_stock=lambda df: df['me'] / df['group_sum'])
     # 4. Zero all portfolio weights for the trading period
-    .assign(w=lambda df: np.where(df['eom'] == trading_start - pd.offsets.MonthEnd(1), df['w'], 0))
-    # 5. Clean up
-    .drop(columns=['group_sum'])
-)
+    .assign(pi_stock=lambda df: np.where(df['eom'] == trading_start - pd.offsets.MonthEnd(1), df['pi_stock'], 0))
+    .assign(pi_choice = 0)
+    .assign(tc = 0)
+    .assign(rev = 0)
+    .merge(df_wealth[['eom','mu', 'wealth']], on = ['eom'], how = 'left')
+    # 5. Calculate 'g'
+    .assign(
+        # Identify the minimum 'eom' date
+        is_min_eom=lambda df: df['eom'] == df['eom'].min(),
+        # Calculate g
+        g=lambda df: np.where(
+            df['is_min_eom'],
+            1,
+            (1 + df['tr']) / (1 + df['mu'])
+        )
+        ) # 5. Clean up
+    .drop(columns=['group_sum', 'is_min_eom'])
+    )
 
 #Read in Barra Covarince matrix
 with open(path + '/Data/Barra_Cov.pkl', "rb") as dict_barra:
@@ -88,11 +107,6 @@ dict_barra = {
     for key, value in dict_barra.items() 
     if key >= trading_start - pd.offsets.MonthEnd(1)  # The condition to KEEP the entry: date_key is NOT less than value
 }
-
-#Read in wealth
-df_wealth = (pd.read_csv(path + "Data/wealth_evolution.csv", parse_dates = ['eom'])
-             .pipe(lambda df: df.loc[df['eom'] >=trading_start - pd.offsets.MonthEnd(1)])
-             )
 
 #%%
 
@@ -130,13 +144,6 @@ def pi_choice(logits_active, pi_stock, active_idx, scaling_coef, ids):
     
     return pd.DataFrame(dictionary)
 
-#Initialise objects
-gt = (pd.DataFrame({'id': df_returns.loc[(df_returns['eom'] == prev_date)]['id'],
-                   'growth':1})
-      .sort_values(by = 'id')
-      .reset_index(drop = True)
-      )
-
 for date in trading_dates:
     
     #=====================================
@@ -150,24 +157,33 @@ for date in trading_dates:
     prev_universe = set(df_pf_weights.loc[df_pf_weights['eom'] == prev_date]['id'])
     cur_universe = set(df_pf_weights.loc[df_pf_weights['eom'] == date]['id'])
     
-    #Stocks that are no longer in the investable universe 
+    #Stocks that can no longer be in the portfolio
     leavers = list(prev_universe - cur_universe)
+    #Stocks that can newly enter the portfolio
     newcomers = list(cur_universe - prev_universe)
+    #Stocks that can remain the portfolio
     stayers = list(cur_universe.intersection(prev_universe))
+    #Stocks which can have non-zero portfolio weights and are active choice variables
+    active = sorted(list(set(newcomers + stayers)))
     
-    #Stocks for which pi_t = 0 must be enforced due to missing data 
-    #on either Kyle's Lambda or the covariance matrix
-    zeros = []
+    #Stocks for which pi_t = 0 must be enforced. This affects all leavers.
+    #On top of that, it can affect a subset of newcomers due to missing data 
+    #on Kyle's Lambda or covariance-matrix
+    zeros = leavers.copy()
     
     #===========================================
     #               Kyle's Lambda
     #===========================================
     
-    #Kyle's Lambda of stayers & leavers
-    kyles_lambda = (df_kl.loc[df_kl.eom == prev_date])[['id','lambda']]
+    #Kyle's Lambda of stayers
+    kyles_lambda = (df_kl.loc[(df_kl['eom'] == prev_date) & 
+                              (df_kl['id'].isin(stayers))
+                              ]
+                    .get(['id','lambda'])
+                    )
     
     #Add newcomers
-    if newcomers:
+    if newcomers: #If newcomers exist, fetch their Kyle's Lambda
         kyles_lambda_newcomers = pd.read_sql_query(("SELECT id, eom, lambda FROM Kyle's Lambda "
                                                  f"WHERE eom = {date} AND id IN ({ids})"
                                                  ),
@@ -177,27 +193,13 @@ for date in trading_dates:
                                                 parse_dates = {'eom'},
                                                 con = JKP_Factors                                    
                                                 )
-        #Add Data on Newcomers
-        kyles_lambda = pd.concat([kyles_lambda,kyles_lambda_newcomers])
+        #Extend zeros with newcomers who don't have data on Kyle's Lambda
+        zeros.extend([stock for stock in newcomers if stock not in set(kyles_lambda['id'])])
+        #Adjust active
+        active = sorted([stock for stock in active if stock not in zeros])
         
-        #Fill zeros with newcomers who don't have data on Kyle's Lambda
-        zeros.extend([n for n in newcomers if n not in set(kyles_lambda['id'])])
-        
-        #Include missing newcomers in dataframe
-        kyles_lambda = (pd.concat([kyles_lambda, 
-                                  pd.DataFrame({'id': zeros,
-                                                'lambda': 0.0*len(zeros)
-                                                })
-                                  ], ignore_index=True)
-                        .sort_values(by = 'id')
-                        .reset_index(drop = True)
-                        )
-        
-    else:
-        kyles_lambda = (kyles_lambda
-                        .sort_values(by = 'id')
-                        .reset_index(drop = True)
-                        )
+        #Add newcomers to Kyle's Lambda
+        kyles_lambda = (pd.concat([kyles_lambda,kyles_lambda_newcomers]))
         
     #===========================================
     #           Barra Covariance Matrix
@@ -206,62 +208,92 @@ for date in trading_dates:
     #Compute covariance matrix
     Sigma = create_cov(dict_barra[prev_date])
        
-    #Only keep stocks in the current stock universe
-    universe = sorted(leavers + stayers  + newcomers)
-    Sigma = Sigma.reindex(index=universe, columns=universe)
+    #Only keep stocks in the active stock universe
+    Sigma = Sigma.reindex(index=active, columns=active)
     
-    #Append zeros with stocks who don't have a variance
-    diagonal = pd.Series(np.diag(Sigma), index=Sigma.index).isna()
-    diagonal = list(diagonal[diagonal].index)
-    zeros.extend(diagonal)
-    Sigma = Sigma.fillna(0)
-    
+    #Remove stocks with missing variances
+    zeros.extend(list(Sigma.index[pd.isna(np.diag(Sigma))]))
+    active = sorted([stock for stock in active if stock not in zeros])
+
     #=====================================
     #        Load Return Prediction
     #=====================================
-    #will always exist for stayers, leavers & newcomers due to Data_Preprocessing
+    
+    #will always exist for stayers & leavers, but not newcomers
     return_predictions = (df_returns
                           .loc[df_returns['eom'] == prev_date]
+                          .get(['id','tr_m_sp500_ld1'])
+                          )
+    zeros.extend([stock for stock in active if stock not in return_predictions['id'].values])
+    active = sorted([stock for stock in active if stock not in zeros])
+    
+    #===========================================
+    #           Only keep active stocks
+    #===========================================
+    
+    #Covariance Matrix (sorted by 'id' as active is sorted)
+    Sigma = Sigma.loc[active, active]
+    
+    #Kyle's Lambda
+    kyles_lambda = (kyles_lambda
+                    .loc[kyles_lambda['id'].isin(active)]
+                    .sort_values(by = 'id')
+                    .reset_index(drop = True)
+                    )
+    
+    #Return predictions
+    return_predictions = (return_predictions
+                          .loc[return_predictions['id'].isin(active)]
                           .sort_values(by = 'id')
                           .reset_index(drop=True)
-                          .get(['id','eom','tr_m_sp500_ld1'])
                           )
+
     
     #==========================================================
     #   Build portfolio stock (pi_t-1) and choice (pi_t) vector
     #==========================================================
     
     #---------- portfolio stock pi_t-1  ----------
-    # Get stayers & leavers
-    pi_stock = (df_pf_weights.loc[df_pf_weights.eom == prev_date][['id','w']]
-            )
+    # 1. Stayers (active stocks)
+    # Get portfolio stock of stayers
+    pi_stock = (df_pf_weights
+                #filter date to t-1
+                .loc[df_pf_weights.eom == prev_date]
+                #Filter out leavers
+                .pipe(lambda df: df.loc[df['id'].isin(active)])
+                #get relevant variables
+                .get(['id','w'])
+                )
     
-    #Add newcomers who have zero stock
-    newcomer_w = {'id': newcomers,
-                     'w': [0.0]*len(newcomers),
-                     }
-    pi_stock = (pd.concat([pi_stock,pd.DataFrame(newcomer_w)])
-              .sort_values(by = 'id')
-              .reset_index(drop = True)
-              .assign(id=lambda df: df['id'].astype(int))
-              )
+    # 2. Newcomers (remaining active stocks)
+    #Add newcomers with zero portfolio weight (no newcomers in df_pf_weights for prev_date)
+    pi_stock = (pd.concat([pi_stock,
+                          pd.DataFrame({'id': [stock for stock in active if stock not in pi_stock.id.values],
+                                        'w': 0.0*len([stock for stock in active if stock not in pi_stock.id.values])
+                                        }
+                                       )
+                          ]
+                         )
+                         .sort_values(by='id')
+                         .reset_index(drop = True)
+                         )
     
     #---------- portfolio choice pi_t ----------
-    # (leavers & newcomers = 0, stayers = w)
-    pi_logits = (pi_stock
-              .assign(logits = lambda df: 
-                      np.where(~df.id.isin(zeros),np.log(df['w']),-np.inf)
-                      )
+    pi_choice = (pi_stock
+              .assign(logits = lambda df: np.log(df['w']))
                   .get(['id','logits'])
                   .sort_values(by = 'id')
                   .reset_index(drop = True)
                   )
     
-    #Optimise only over active logits
-    pi_logits.loc[~pi_logits['id'].isin(zeros)]
-    active_idx = list(pi_logits.loc[~pi_logits['id'].isin(zeros)].index)
-    logits_active = pi_logits['logits'].iloc[active_idx]
+    #==========================================================
+    #           Solve for optimal portfolio vector
+    #==========================================================
     
+    !!!!
+    HIER MUSS ICH gt pi_{t-1} jetzt inkorporieren und prinzipiell nochmal checken,
+    dass mein portfolio choice auch wirklich richtig formuliert ist
+    !!!!
     logits_BFGS = minimize(portfolio_return_BFGS, logits_active.to_numpy(),
                            args = (pi_stock['w'].to_numpy(),
                                    Sigma.to_numpy(),
@@ -278,6 +310,9 @@ for date in trading_dates:
     pi_opt = pi_choice(logits_BFGS.x, pi_stock['w'].to_numpy(), active_idx, 
                    np.sqrt(len(logits_active)), pi_stock['id'].to_numpy()
                    )
+    pi_choice['w_choice'] = softmax pi_opt['logits']
+    pi_choice['eom'] = prev_date
+    df_pf_weights = df_pf_weights.merge(pi_choice, on = ['id', 'eom'], how = 'left')
     
     #Update Portfolio weights given their returns
     gt = (
