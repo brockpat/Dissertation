1ca84044dd46066da242f410b03889d5f2c8c6f7
Patrick Brock <patrick.brock94@gmx.de>
2025-11-24 14:39:24 +0100

    Create XGBoost_Regressor.py

---
+++ Machine Learning Portfolio Selection/XGBoost_Regressor.py
@@ -0,0 +1,302 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 20 09:48:48 2025
+
+@author: pbrock
+"""
+
+#%% Libraries
+
+path = "C:/Users/pbrock/Desktop/ML/"
+
+#DataFrame Libraries
+import pandas as pd
+import sqlite3
+
+#Turn off pandas performance warnings
+import warnings
+warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
+
+#Plot Libraries
+import matplotlib.pyplot as plt
+
+#Scientifiy Libraries
+import numpy as np
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error
+import optuna
+
+import os
+os.chdir(path + "Code/")
+import General_Functions as GF
+
+#%% Preliminaries & Data
+
+#=================================================
+#               Preliminaries
+#=================================================
+
+#Database
+JKP_Factors = sqlite3.connect(database=path +"Data/JKP_clean.db")
+db_Predictions = sqlite3.connect(database=path +"Data/Predictions.db")
+
+
+#Get Settings & signals
+settings = GF.get_settings()
+signals = GF.get_signals()
+feat_cols = signals[0] + signals[1]
+
+#Window Size and Validation Periods for rolling window
+window_size = 120 #10 years of data
+validation_size = 12 #1 year of data
+test_size = 12 #Periods until hyperparameters are re-tuned. Fine-tuning is done monthly
+
+file_end = f"LevelTarget_CRSPUniverse_RankFeatures_RollingWindow_win{window_size}_val{validation_size}_test{test_size}"
+
+#Trading Dates
+trading_dates = pd.date_range(settings['rolling_window']['trading_month'], "2024-12-31", freq = 'ME')
+
+#=================================================
+#               Read in Data
+#=================================================
+
+#Read in processed signals
+query = ("SELECT * FROM Signals_Rank "
+         f"WHERE eom >= '{(trading_dates[0]-pd.offsets.MonthEnd(window_size+validation_size+1)).strftime("%Y-%m-%d")}'")
+df_signals = pd.read_sql_query(query,
+                               con = JKP_Factors,
+                               parse_dates = {'eom'})
+
+#Read in Targets
+query = ("SELECT id, eom, tr_m_sp500_ld1 FROM Factors_processed "
+         f"WHERE eom >= '{(trading_dates[0]-pd.offsets.MonthEnd(window_size+validation_size+1)).strftime("%Y-%m-%d")}'")
+df_targets = pd.read_sql_query(query,
+                               con = JKP_Factors,
+                               parse_dates = {'eom'})
+    
+
+#Merge (Merging in Python was quicker than via SQL)
+df_signals = (df_signals
+              .merge(df_targets, on = ['id','eom'], how = 'left')
+              .dropna(subset = 'tr_m_sp500_ld1')
+              .sort_values(by = ['eom','id'])
+              )
+
+if df_signals['tr_m_sp500_ld1'].isna().sum() > 0:
+    print("ERROR: Missing Values in Target. Training cannot be initiated")
+del df_targets
+
+
+#Extract unique Months of signals
+signal_months = df_signals['eom'].sort_values().unique()
+
+#Extract index of trading start
+trade_idx = signal_months.searchsorted(trading_dates[0])
+
+
+#%% XGBoost Preparation
+
+#=================================================
+#               XGBoost Regression Tree
+#=================================================
+
+#XGBoost base parameters
+base_params = dict(
+    objective="reg:squarederror", # L2 loss
+    tree_method="hist",           # or "approx" / "gpu_hist" if GPU
+    learning_rate=0.05,
+    n_estimators=300,
+    max_depth=3,
+    min_child_weight=5,
+    subsample=0.8,
+    colsample_bytree=0.8,
+    gamma=0.0,
+    reg_alpha=0,  # no L1
+    reg_lambda=1.0, # L2
+    n_jobs=-1,
+    random_state=2025,
+)
+
+#Optuna Objective Function
+def make_objective(X_train, y_train, X_val, y_val):
+
+    def objective(trial):
+            params = {
+                "objective": "reg:squarederror",
+                "tree_method": "hist",
+                "n_jobs": -1,
+                "random_state": 42,
+                "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.2, log=True),
+                "max_depth": trial.suggest_int("max_depth", 2, 8),
+                "min_child_weight": trial.suggest_float("min_child_weight", 1.0, 10.0),
+                "subsample": trial.suggest_float("subsample", 0.5, 1.0),
+                "colsample_bytree": trial.suggest_float("colsample_bytree", 0.5, 1.0),
+                "gamma": trial.suggest_float("gamma", 0.0, 5.0),
+                "reg_alpha": 0,  # no L1
+                "reg_lambda": trial.suggest_float("reg_lambda", 0.1, 20.0, log=True),
+                
+                # FIX n_estimators (M) to a high value high; early stopping decides the effective number of M
+                "n_estimators": 500,
+                "eval_metric": "rmse",
+                'early_stopping_rounds': 50
+            }
+    
+            model = XGBRegressor(**params)
+    
+            model.fit(
+                X_train, y_train,
+                eval_set=[(X_val, y_val)],
+                verbose=False,
+            )
+            
+            # Save best_iteration for this trial (number of trees M)
+            trial.set_user_attr("best_iteration", model.best_iteration)
+    
+            y_val_pred = model.predict(X_val)
+            mse = mean_squared_error(y_val, y_val_pred)
+            return mse  # Optuna will minimize this
+        
+    return objective
+
+#%% Rolling Window Estimation
+
+#Empty list to store the predictions
+predictions = []
+
+# Track how many periods since last hyperparameter tuning
+months_since_tune = test_size   # start so that the first iteration triggers tuning
+saved_best_params = None
+saved_best_n_estimators = None
+
+#Loop over dates (Rolling Window)
+for date in trading_dates:
+    
+    #========================================
+    # Get the Data for the Rolling Regression
+    #========================================
+    
+    #Rolling Window Dates
+    start_idx = max(0, trade_idx - (window_size + validation_size+1))
+    window_months = signal_months[start_idx:trade_idx-1]
+    #Note: we can only use signals up to t-2 since we are predicting next period's targelt. Else-wise: Look-ahead bias (data leakage)
+    
+    # Split into train / val months
+    train_months = window_months[:window_size]
+    val_months   = window_months[window_size:]
+    
+    # Training Data
+    X_train = df_signals[df_signals['eom'].isin(train_months)][feat_cols].to_numpy()
+    y_train = df_signals[df_signals['eom'].isin(train_months)]['tr_m_sp500_ld1'].to_numpy()
+    
+    #Validation Data
+    X_val = df_signals[df_signals['eom'].isin(val_months)][feat_cols].to_numpy()
+    y_val = df_signals[df_signals['eom'].isin(val_months)]['tr_m_sp500_ld1'].to_numpy()
+    
+    
+    #======================================================
+    #     Hyperparameter tuning every `test_size` months
+    #======================================================
+    if months_since_tune >= test_size:
+    
+        print(f"Re-tuning hyperparameters at date {date}...")
+    
+        # Run Optuna tuning
+        study = optuna.create_study(direction="minimize")
+        objective = make_objective(X_train, y_train, X_val, y_val)
+        study.optimize(objective, n_trials=15)
+    
+        # Extract optimal hyperparameters
+        best_params = study.best_params.copy()
+    
+        # Add fixed parameters
+        best_params.update({
+            "objective": "reg:squarederror",
+            "tree_method": "hist",
+            "n_jobs": -1,
+            "random_state": 2025,
+            "reg_alpha": 0.0,
+            "eval_metric": "rmse"
+        })
+    
+        # Retrieve optimal number of trees from early stopping
+        best_iteration = study.best_trial.user_attrs["best_iteration"]
+        best_n_estimators = best_iteration + 1
+        best_params["n_estimators"] = best_n_estimators
+        print(f"Number of trees used for final model at date {date}: {best_n_estimators}")
+
+    
+        # Store hyperparameters for future months
+        saved_best_params = best_params.copy()
+        saved_best_n_estimators = best_n_estimators
+    
+        months_since_tune = 0  # reset counter
+    
+    else:
+        # Reuse previously tuned hyperparameters
+        best_params = saved_best_params.copy()
+        best_params["n_estimators"] = saved_best_n_estimators
+        print(f"Skipping hyperparameter tuning at date {date}, reusing saved params.")
+    
+    # Increment months since last tune
+    months_since_tune += 1
+
+
+    #===========================================================
+    #           Refit on Train & Validation Data
+    #===========================================================
+    
+    #Get Train + Val Data
+    window_mask = df_signals['eom'].isin(window_months)
+    X_window = df_signals.loc[window_mask, feat_cols]
+    y_window = df_signals.loc[window_mask, 'tr_m_sp500_ld1']
+    
+    #Train the Model given the hyperparameters
+    final_model = XGBRegressor(**best_params)
+    final_model.fit(
+        X_window, y_window
+    )    
+        
+    
+    #===========================================================
+    #           Predict next month's OOS Return
+    #===========================================================
+    
+    # Make the next month prediction for the out-of-sample period
+    test_mask = (df_signals['eom'] == date-pd.offsets.MonthEnd(1))
+    X_test = df_signals.loc[test_mask, feat_cols]
+    ids_test = df_signals.loc[test_mask, ['id', 'eom']]
+    
+    y_test_pred = final_model.predict(X_test)    
+    
+    
+    #===========================================================
+    #                   Save results
+    #===========================================================
+    
+    #At 'eom', predict return for 'eom'+1
+    pred_df = ids_test.copy()
+    pred_df['ret_pred'] = y_test_pred
+    predictions.append(pred_df)
+    
+    # Save the model for this date
+    model_path = os.path.join(path, "Models/XGBoost/", f"xgb_{file_end}_{date.strftime('%Y%m%d')}.json")
+    final_model.save_model(model_path)
+    
+    # Dump tree structure in text format
+    tree_path = os.path.join(path, "Models/XGBoost/", f"xgb_trees_{file_end}_{date.strftime('%Y%m%d')}.txt")
+    final_model.get_booster().dump_model(tree_path)
+
+
+    #Increment index due to new trading_month
+    trade_idx += 1
+
+#%% Save Predictions
+df_predictions = pd.concat(predictions)
+
+df_predictions.to_sql(name = f"XGBoost_{file_end}",
+                   con = db_Predictions,
+                   index = False,
+                   if_exists = 'append')
+
+JKP_Factors.close()
+db_Predictions.close()
